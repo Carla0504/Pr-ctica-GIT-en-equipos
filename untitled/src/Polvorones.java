e02960531b983dfa12d3da306c12acc3f2618d20
Carla0504 <carla.fernandez86@educa.madrid.org>
2025-02-10 10:02:41 +0100

    Primer cambio

--- untitled/src/Polvorones.java
+++ untitled/src/Polvorones.java
@@ -2,22 +2,22 @@ import java.util.*;
 
 public class Polvorones {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+        Scanner teclado = new Scanner(System.in);
 
         while (true) {
-            int limiteMedico = sc.nextInt();
-            int tipos = sc.nextInt();
+            int limiteMedico = teclado.nextInt();
+            int tipos = teclado.nextInt();
 
             if (limiteMedico == 0 && tipos == 0) break;
 
             int[] limitesPersonales = new int[tipos];
             for (int i = 0; i < tipos; i++) {
-                limitesPersonales[i] = sc.nextInt();
+                limitesPersonales[i] = teclado.nextInt();
             }
 
             int[] bandeja = new int[tipos];
             for (int i = 0; i < tipos; i++) {
-                bandeja[i] = sc.nextInt();
+                bandeja[i] = teclado.nextInt();
             }
 
             int totalComido = 0;
@@ -38,6 +38,6 @@ public class Polvorones {
             System.out.println(posible ? "SI" : "NO");
         }
 
-        sc.close();
+        teclado.close();
     }
 }
